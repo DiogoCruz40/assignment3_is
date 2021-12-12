34a8f19af35eae653a80b297eca22547ad469f96
Diogo Cruz <diogo.e.cruz@hotmail.com>
2021-12-12 18:10:18 +0000

    creditperclient

--- KafkaStreams/src/main/java/dtos/CreditsDTO.java
+++ KafkaStreams/src/main/java/dtos/CreditsDTO.java
@@ -8,11 +8,13 @@ public class CreditsDTO implements Serializable {
     private long id_manager;
     private long id_client;
     private double valueofcredit;
+    private double exchangeRate;
 
-    public CreditsDTO(long id_manager, long id_client, double valueofcredit) {
+    public CreditsDTO(long id_manager, long id_client, double valueofcredit, double exchangeRate) {
         this.id_manager = id_manager;
         this.id_client = id_client;
         this.valueofcredit = valueofcredit;
+        this.exchangeRate = exchangeRate;
     }
 
     public long getId_client() {
@@ -37,6 +39,14 @@ public class CreditsDTO implements Serializable {
 
     public void setId_manager(long id_manager) {
         this.id_manager = id_manager;
+    }
+
+    public double getExchangeRate() {
+        return exchangeRate;
+    }
+
+    public void setExchangeRate(double exchangeRate) {
+        this.exchangeRate = exchangeRate;
     }
 }
 
